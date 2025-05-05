0ec4cc36df64534127fc8bcc1801df1d0a21a0f0
Neeza Singh <neezasingh@Mac.cable.rcn.com>
2025-05-05 02:06:30 -0400

    Fix: use app.run instead of app.run_server for Dash 3+

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def render_content(tab):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
