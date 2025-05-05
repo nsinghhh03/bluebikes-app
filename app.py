e9e6f02ac22b61686ed540ba5d83067e876ef8e6
Neeza Singh <neezasingh@Mac.cable.rcn.com>
2025-05-05 01:49:31 -0400

    port update

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def render_content(tab):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8050))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run_server(host="0.0.0.0", port=port)
